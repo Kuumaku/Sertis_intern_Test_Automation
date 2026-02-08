a394bd31bbe074bb35b6d977d9a75ba49d6084a5
Kuumaku <kimberry2547@gmail.com>
2026-02-08 08:22:20 +0700

    Commenting logic

--- tests/ToCertis.spec.js
+++ tests/ToCertis.spec.js
@@ -1,5 +1,13 @@
+//Create Playwright environment
+//Create test name Search and enter certis
+//1.Go to google.com
+//2.Search for combobox by role and create const as search bar
+//3.fill Sertis in search bar
+//4.Press Enter
+//5.See if sertis URL are visible in search results page
+
 import {test, expect} from '@playwright/test';
-``
+
 
 test('Search and enter certis ', async ({page}) => { 
 
@@ -13,8 +21,6 @@ test('Search and enter certis ', async ({page}) => {
     await expect(search_bar).toHaveValue(`Sertis`)
     
     await search_bar.press('Enter')
-
-    await page.waitForLoadState("networkidle");
 
     await expect(page.getByText(`https://www.sertiscorp.com/`)).toBeVisible;
 
